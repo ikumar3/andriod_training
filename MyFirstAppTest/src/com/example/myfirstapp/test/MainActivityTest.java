b7099897ec00488bc61dd065386712781a0a64b8
lilinj2000 <lilinj2000@163.com>
2014-12-01 09:43:14 +0000

    small update

--- MyFirstAppTest/src/com/example/myfirstapp/test/MainActivityTest.java
+++ MyFirstAppTest/src/com/example/myfirstapp/test/MainActivityTest.java
@@ -1,10 +1,9 @@
 package com.example.myfirstapp.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
 import com.example.myfirstapp.MainActivity;
 import com.example.myfirstapp.R;
 
@@ -13,6 +12,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
 	private MainActivity mainActivity;
     private EditText editMessage;
+    private Button buttonSend;
     
 
     public MainActivityTest() {
@@ -27,17 +27,31 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         editMessage =
                 (EditText) mainActivity
                 .findViewById(R.id.edit_message);
+        
+        buttonSend = (Button) mainActivity
+        		.findViewById(R.id.button_send);
     }
     
+    @SmallTest
     public void testPreconditions() {
         assertNotNull("mainActivity is null", mainActivity);
         assertNotNull("editMessage is null", editMessage);
+        assertNotNull("buttonSend is null", buttonSend);
     }
     
+    @SmallTest
     public void testEditMessageHint() {
         final String expected =
         		mainActivity.getString(R.string.edit_message);
         final String actual = editMessage.getHint().toString();
+        assertEquals(expected, actual);
+    }
+    
+    @SmallTest
+    public void testButtonSendText() {
+        final String expected =
+        		mainActivity.getString(R.string.button_send);
+        final String actual = buttonSend.getText().toString();
         assertEquals(expected, actual);
     }
     
